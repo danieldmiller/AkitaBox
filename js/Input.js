5e618fc7ddee1879b3383054ffb5ecb4078bde2c
Daniel Miller <ddmiller3@wisc.edu>
2018-03-13 22:57:04 -0500

    Globalize getInputAs2dArray() to allow it to be called from Main.js

--- js/Input.js
+++ js/Input.js
@@ -9,28 +9,26 @@
  * @returns {Number[][]} result - 2d-array of cells
  */
 
-(function() {
-    function getInputAs2dArray(input) {
-        const result = [];
-        const arr = input.split("\n");
-        for(let i=0; i<arr.length; i++) {
-            let row = arr[i];
-            let cells = row.split("").map(Number); //convert String[] into Number[]
-            for(let cell=0; cell<cells.length; cell++) {
-                if(isNaN(cells[cell])) {
-                    return;
-                }
+function getInputAs2dArray(input) {
+    const result = [];
+    const arr = input.split("\n");
+    for(let i=0; i<arr.length; i++) {
+        let row = arr[i];
+        let cells = row.split("").map(Number); //convert String[] into Number[]
+        for(let cell=0; cell<cells.length; cell++) {
+            if(isNaN(cells[cell])) {
+                return;
             }
-            result.push(cells);
         }
-        return result;
+        result.push(cells);
     }
+    return result;
+}
 
-    /** Click listener for rpentomino button to populate textarea with an R-Pentomino board */
-    document.querySelector("#rpentomino").addEventListener("click", () => {
-        const rpentomino = "000000000000000\n000000000000000\n000000000000000\n000000000000000\n000000000000000\n000000000000000\n000000011000000\n000000110000000\n000000010000000\n000000000000000\n000000000000000\n000000000000000\n000000000000000\n000000000000000\n000000000000000";
-        const textArea = document.querySelector("textarea");
-        textArea.style.height = "205px";
-        textArea.value = rpentomino;
-    });
-})();
+/** Click listener for rpentomino button to populate textarea with an R-Pentomino board */
+document.querySelector("#rpentomino").addEventListener("click", () => {
+    const rpentomino = "000000000000000\n000000000000000\n000000000000000\n000000000000000\n000000000000000\n000000000000000\n000000011000000\n000000110000000\n000000010000000\n000000000000000\n000000000000000\n000000000000000\n000000000000000\n000000000000000\n000000000000000";
+    const textArea = document.querySelector("textarea");
+    textArea.style.height = "205px";
+    textArea.value = rpentomino;
+});
